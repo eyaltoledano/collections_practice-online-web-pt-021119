61630f6f7f6f81b02b8a1b436c3877a8bbad6ac6
Eyal Toledano <eyaltoledano@me.com>
2019-02-18 19:36:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  new = array.sort { |str| str.size }
+  binding.pry
+end
